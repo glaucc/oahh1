49afb5e4d47fefc4de3de5df49b6b7751af1d354
glaucc <ahmedovyusif8785@gmail.com>
2023-01-18 17:18:40 +0400

    oah route

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Home from './pages/Home'
 import Handstand from './pages/Handstand';
+import Oah from './pages/oah';
 import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
@@ -19,11 +20,19 @@ function App() {
     },
   ]);
 
+  const oah = createBrowserRouter([
+    {
+      path: "/oah",
+      element: <Oah />,
+    },
+  ]);
+
   return (
     <div className="App">
 
       <RouterProvider router={home} />
       <RouterProvider router={handstand} />
+      <RouterProvider router={oah} />
       {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>
---
+++ react/src/pages/oah.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Oah() {
+    return (
+        <>
+            <h1>aa</h1>
+        </>
+    )
+}
