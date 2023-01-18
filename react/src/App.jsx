e0110abdcc3ffb6b2b628a7327f0ac2a37933dbb
Puffy <88508824+PufflyMan@users.noreply.github.com>
2023-01-18 04:42:52 +0400

    routing 1

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
 import Home from './pages/Home'
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
 
+  const home = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
   return (
     <div className="App">
-      <Home />
+
+      <RouterProvider router={home} />
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter> */}
     </div>
   )
 }
